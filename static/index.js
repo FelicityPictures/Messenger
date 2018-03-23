d9fb383bd79ea89c21f4d9a6773cf7a78cd29b9b
Felicity Ng <doodlefelicity@gmail.com>
2018-03-23 18:48:45 +0800

    small visual changes

--- static/index.js
+++ static/index.js
@@ -1,20 +1,28 @@
 var sendButton = document.getElementById('sendButton');
 var textMessage = document.getElementById('inputText');
-sendButton.addEventListener("click", sendMessage);
+var messages = document.getElementById('messages');
+// sendButton.addEventListener("click", sendMessage);
 
 var socket = io();
 socket.on('connect', function() {
   console.log('connected');
 });
 
+$("#inputForm").submit((e) => {
+  e.preventDefault();
+  sendMessage();
+})
+
 // get message
-function sendMessage(){console.log('sending message');
+function sendMessage(){
+  console.log('sending message');
   var messageText = textMessage.value;
   socket.emit('message', messageText);
   document.getElementById("inputText").value = '';
 }
 
 // receives messages
+
 socket.on('new_message', (data) => {
   var node = document.createElement("LI");
   var textnode = document.createTextNode(data);
